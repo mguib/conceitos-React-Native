3b7197fdc05b28174720e2244e0c65b94e00e20f
Marcos Guibson <mgss.lic17@uea.edu.br>
2022-01-31 15:21:43 -0400

    feat: Finising basic concpts challenge in React Native

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,14 +9,36 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
     //TODO - add new task
+
+    setTasks(oldTasks => [...oldTasks ,newTask])
   }
 
   function handleToggleTaskDone(id: number) {
+    //Criando modificação na task selecionada
+    const updatedTasks = tasks.map(task => ({ ...task }))
+
+    //Buscando task na lista de tasks nova
+    const searchItem = updatedTasks.find(item => item.id === id);
+    if(!searchItem){
+      return
+    }
+    //Modificando propriedade
+    searchItem.done = !searchItem.done;
+    //Atualizando estado
+    setTasks(updatedTasks);
     //TODO - toggle task done if exists
   }
 
   function handleRemoveTask(id: number) {
+    setTasks(oldState => oldState.filter(
+      task => task.id != id
+    ))
     //TODO - remove task from state
   }
 
